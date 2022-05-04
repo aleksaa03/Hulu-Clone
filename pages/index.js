56ead39a7fe06e6ed38834faedca857c5be3090c
Aleksa <71262651+aleksaa03@users.noreply.github.com>
2022-05-04 15:42:25 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
+
+export default function Home({ results }) {
+  console.log(results);
+  return (
+    <div>
+      <Head>
+        <title>Hulu</title>
+        <meta name="description" content="Hulu" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Nav />
+      <Results results={results} />
+    </div>
+  );
+}
+
+//gidabap686@bunlets.com
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+}
